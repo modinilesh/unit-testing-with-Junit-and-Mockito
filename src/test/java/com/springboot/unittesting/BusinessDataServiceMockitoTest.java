891a178ce8e676a9bdcd3e60a9847155ae9cd513
Nilesh Modi <nilesh.modi.nitb@gmail.com>
2024-03-23 02:55:11 +0530

    Reduced Redundant Code
    Reduced Redundant Code

--- src/test/java/com/springboot/unittesting/BusinessDataServiceMockitoTest.java
+++ src/test/java/com/springboot/unittesting/BusinessDataServiceMockitoTest.java
@@ -4,70 +4,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.springboot.unittesting.Business.BusinessImpl;
 import com.springboot.unittesting.service.DataService;
 
-
-
 public class BusinessDataServiceMockitoTest {
 
-    @Test
-    void calculateSumUsingDataService_basic() {
-        BusinessImpl business = new BusinessImpl();
-        
-        //let's mock the Data Service
-        DataService mockDataService = mock(DataService.class);
-        
-        //return data based on requirements
-        when(mockDataService.retrieveAllData()).thenReturn(new int[] {1, 2, 3});
-        
-        //setting data service
-        business.setDataService(mockDataService);
-        
-        //getting actual results
-        int actualResult = business.calculateSumUsingDataService();
-        int expectedResult = 6;
-        assertEquals(expectedResult, actualResult);
-    }
-    
+	// To reduce the redundant code
+	BusinessImpl business = new BusinessImpl();
+	DataService mockDataService = mock(DataService.class);
 
-    @Test
-    void calculateSumUsingDataService_empty() {
-        BusinessImpl business = new BusinessImpl();
-        
-        //let's mock the Data Service
-        DataService mockDataService = mock(DataService.class);
-        
-        //return data based on requirements
-        when(mockDataService.retrieveAllData()).thenReturn(new int[] {});
-        
-        //setting data service
-        business.setDataService(mockDataService);
-        
-        //getting actual results
-        int actualResult = business.calculateSumUsingDataService();
-        int expectedResult = 0;
-        assertEquals(expectedResult, actualResult);
-    }
-    
-    @Test
-    void calculateSumUsingDataService_singleValue() {
-        BusinessImpl business = new BusinessImpl();
+	// setting data service
+	@BeforeEach
+	public void beforeTest() {
+		business.setDataService(mockDataService);
+	}
 
-        //let's mock the Data Service
-        DataService mockDataService = mock(DataService.class);
-        
-        //return data based on requirements
-        when(mockDataService.retrieveAllData()).thenReturn(new int[] {1});
-        
-        //setting data service
-        business.setDataService(mockDataService);
-        
-        //getting actual results
-        int actualResult = business.calculateSumUsingDataService();
-        int expectedResult = 1;
-        assertEquals(expectedResult, actualResult);
-    }
+	@Test
+	void calculateSumUsingDataService_basic() {
+
+		// return data based on requirements
+		when(mockDataService.retrieveAllData()).thenReturn(new int[] { 1, 2, 3 });
+		assertEquals(6, business.calculateSumUsingDataService());
+	}
+
+	@Test
+	void calculateSumUsingDataService_empty() {
+		// return data based on requirements
+		when(mockDataService.retrieveAllData()).thenReturn(new int[] { });
+		assertEquals(0, business.calculateSumUsingDataService());
+	}
+
+	@Test
+	void calculateSumUsingDataService_singleValue() {
+		// return data based on requirements
+		when(mockDataService.retrieveAllData()).thenReturn(new int[] { 1 });
+		assertEquals(1, business.calculateSumUsingDataService());
+	}
 
 }
