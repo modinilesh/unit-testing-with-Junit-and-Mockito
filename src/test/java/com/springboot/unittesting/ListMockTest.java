829d0ccab5908b07bab2d3c51171aeac271fdc35
Nilesh Modi <nilesh.modi.nitb@gmail.com>
2024-03-24 01:27:27 +0530

    Mockito - Verify Methods
    verifying that if one mock is calling another mock/methods

--- src/test/java/com/springboot/unittesting/ListMockTest.java
+++ src/test/java/com/springboot/unittesting/ListMockTest.java
@@ -2,6 +2,11 @@ package com.springboot.unittesting;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -37,11 +42,31 @@ class ListMockTest {
 		assertEquals(null, mockList.get(0));
 	}
 	
+	
+	//Argument Matchers
 	@Test
 	public void returnWithGenericParameter() {
 		when(mockList.get(anyInt())).thenReturn("Nilesh Modi"); 	
 		assertEquals("Nilesh Modi", mockList.get(1));
 		assertEquals("Nilesh Modi", mockList.get(0));
+	}
+	
+	
+	//Verifying Methods -> verifying that if one mock is calling another mock/method
+	@Test
+	public void verificationBasic() {
+		
+		//suppose in below statements mock is calling another method .get()
+		String value1 = (String) mockList.get(0);
+		String value2 = (String) mockList.get(1);
+		
+		//now, verifying above, i.e. is mock calling to another method
+		verify(mockList).get(0); //does mocklist call get(0) //true
+		verify(mockList, times(2)).get(anyInt());	//does mocklist call get(integer) two times  //true
+		verify(mockList, atLeast(1)).get(anyInt()); //does mocklist call get(integer) atleast one time //true
+		verify(mockList, atMost(2)).get(anyInt());
+		verify(mockList, never()).get(2);
+		
 	}
 	
 	
